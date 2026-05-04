e25c16a2e163d39002ec033b46f0516eae50a0ad
Sushant <sushant.dhiman9812@gmail.com>
2026-05-04 08:11:58 +0530

    tcp server

--- main.c
+++ main.c
@@ -1,10 +1,14 @@
+#include <stdlib.h>
 #include <errno.h>
 #include <netinet/in.h>
 #include <stdio.h>
 #include <string.h>
 #include <sys/socket.h>
+#include <unistd.h>
 
 #define PORT 8080
+#define BACKLOG_SIZE 128
+#define MSG_MAX_SIZE 1024
 
 int main(void) {
     struct sockaddr_in server_sockaddr_in;
@@ -20,21 +24,71 @@ int main(void) {
     */
     server_sockaddr_in.sin_addr.s_addr = htonl(INADDR_ANY);
 
-    // htons converts host byte order to network byte order for unsigned short
-    // integer.
+    /*htons converts host byte order to network byte order for unsigned short integer. */
     server_sockaddr_in.sin_port = htons(PORT);
 
     int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (socket_fd == -1) {
         printf("unable to create socket: %s", strerror(errno));
+        exit(-1);
     }
 
     int res = bind(socket_fd, (struct sockaddr *)&server_sockaddr_in, sizeof(server_sockaddr_in));
     if (res == -1) {
         printf("unable to bind socket: %s", strerror(errno));
+        exit(-1);
     }
 
-    listen(socket_fd, 5);
+    printf("listening on port: %d\n", PORT);
+
+    /*
+     * When a server starts listening for connections, the kernel maintains internal queues to manage incoming TCP requests
+     * before the application officially "accepts" them with the accept() call.
+     */
+    res = listen(socket_fd, BACKLOG_SIZE);
+    if (res == -1) {
+        printf("unable to listen on socket: %s", strerror(errno));
+        exit(-1);
+    }
+
+    struct sockaddr_in client_addr;
+    socklen_t len = sizeof(client_addr);
+
+    while (1) {
+        int conn_fd = accept(socket_fd, (struct sockaddr*)&client_addr, &len);
+        printf("connection accepted\n");
+
+        char buffer[MSG_MAX_SIZE];
+
+        while (1) {
+            memset(buffer, 0, sizeof(buffer));
+
+            int n = read(conn_fd, buffer, sizeof(buffer) - 1);
+            if (n < 0) {
+                printf("unable to read from client: %s", strerror(errno));
+                exit(-1);
+            }
+
+            buffer[n] = '\0';
+
+            printf("client sent: %s\n", buffer);
+
+            if (strncmp(buffer, "exit", 4) == 0) {
+                char* msg = "closing connection";
+                int n = write(conn_fd, msg, strlen(msg));
+
+                if (n == -1) {
+                    printf("unable to write: %s", strerror(errno));
+                }
+
+                if (close(conn_fd) == -1) {
+                    printf("unable to close connection: %s", strerror(errno));
+                }
+
+                break;
+            }
+        }
+    }
 
     return 0;
 }
