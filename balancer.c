1b2aefeac8828e6e61d00d1ff3b76e02e735b1f5
Sushant <sushant.dhiman9812@gmail.com>
2026-05-05 21:29:52 +0530

    converted socket_fd to non-blocking mode

--- balancer.c
+++ balancer.c
@@ -5,10 +5,22 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 #define PORT 8080
 #define BACKLOG_SIZE 128
 #define MSG_MAX_SIZE 1024
+
+/*
+ * This function enabled non-blocking mode on any file descriptor.
+ * Syscalls like read(), write(), accept() are blocking by default. If there is no data program waits (get stucked).
+ * In non-blocking mode these functions returns instantly instead of waiting.
+ */
+int set_non_blocking(int fd) {
+    int flags = fcntl(fd, F_GETFL, 0);
+    // flags | O_NONBLOCK uses a bitwise OR operator which keeps existing flags and add the new.
+    return fcntl(fd, F_SETFL, flags | O_NONBLOCK);
+}
 
 int main(void) {
     struct sockaddr_in server_sockaddr_in;
@@ -18,7 +30,7 @@ int main(void) {
     /*
     * htonl() converts host byte order to network byte order. l mean unsigned
     * long integer.
-    *f
+    *
     * INADDR_ANY bind socket to all available network interfaces of
     * a mahcine. eg WiFi, Ethernet
     */
@@ -30,6 +42,11 @@ int main(void) {
     int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
     if (socket_fd == -1) {
         printf("unable to create socket: %s", strerror(errno));
+        exit(-1);
+    }
+
+    if (set_non_blocking(socket_fd) == -1) {
+        printf("unable to enable non blocking mode on socket: %s", strerror(errno));
         exit(-1);
     }
 
@@ -70,23 +87,6 @@ int main(void) {
             }
 
             buffer[n] = '\0';
-
-            printf("client sent: %s\n", buffer);
-
-            if (strncmp(buffer, "exit", 4) == 0) {
-                char* msg = "closing connection";
-                int n = write(conn_fd, msg, strlen(msg));
-
-                if (n == -1) {
-                    printf("unable to write: %s", strerror(errno));
-                }
-
-                if (close(conn_fd) == -1) {
-                    printf("unable to close connection: %s", strerror(errno));
-                }
-
-                break;
-            }
         }
     }
 
